2f55b5197f83f02bb3aac2ea16eff9335384dc61
sarinderv <sarinderv@users.noreply.github.com>
2022-04-13 20:58:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'otto.db.elephantsql.com',
+    host: env.DB_HOST || 'heffalump.db.elephantsql.com',
     port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'cklijfef',
-    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-    database: env.DB_NAME || 'cklijfef',
+    user: env.DB_USER || 'fhdmjxva',
+    password: env.DB_PASSWORD || 'w3vlGlT7zdtW25mvdiCduSHsYhbv2ij9',
+    database: env.DB_NAME || 'fhdmjxva',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
